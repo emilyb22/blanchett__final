8fdbe98de3c1ec57329dea8f99d070f707366873
emilyb22 <emilyb22@live.ca>
2017-08-09 17:52:58 -0400

    firebase config

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
-angular.module('ToDoApp',[]). /*App controller*/
-controller('toDoListCrtl',['$scope','apptodoLocalStorage', function($scope, apptodoLocalStorage){ /*displaying list controller*/
-    $scope.todoitemsList = apptodoLocalStorage.get("item") || [];
+angular
+	.module('contactsApp', ['firebase'])
+    .constant('firebaseConfig', {
+     apiKey: "AIzaSyBl1hDAyp38K3WNeZljOl5Z4fMGayyLSi8",
+    authDomain: "final-f22f0.firebaseapp.com",
+    databaseURL: "https://final-f22f0.firebaseio.com",
+    projectId: "final-f22f0",
+    storageBucket: "final-f22f0.appspot.com",
+    messagingSenderId: "149695883160"  
+})
+.run(firebaseConfig => firebase.initializeApp(firebaseConfig))
+.controller('contactsCtrl', function($scope, $firebaseObject, $firebaseArray){
+	const dbRef = firebase.database().ref().child('contacts')
     
-    $scope.addTodoItem = function(){ /*Adding items through the formy input*/
-        $scope.todoitemsList.push({'title':$scope.newTask,'date':$scope.newDate, 'done':false}) //adding the date and the title
-        apptodoLocalStorage.set("item", $scope.todoitemsList)
-        apptodoLocalStorage.set("done", $scope.todoitemsList)
-        $scope.newTask = ''/*To empty the input*/
-        $scope.newDate = ''
+    $scope.contactList = $firebaseArray(dbRef)
+    
+    $scope.contact = $firebaseObject(dbRef.child('-KoO9a3E2avl-qdnxNU7'))
+    
+    this.getBlankContact = () => ({
+        fname:'',
+        lname: '',
+        phone: '',
+        email: '',
+        birthday: '',
+        isCompleted: false
+    })
+    
+    $scope.newContact = this.getBlankContact()
+    
+    $scope.addContact = () => {
+        $scope.contactList.$add($scope.newContact)
+        $scope.newContact = this.getBlankContact()
     }
     
-     
-    $scope.clearCompleted = function(){ /*Delete all completed function*/
-            $scope.todoitemsList = $scope.todoitemsList.filter(function(item){
-            return !item.done
-        })
-            apptodoLocalStorage.remove('item', $scope.todoitemsList)
-            apptodoLocalStorage.set('item', $scope.todoitemsList)
+    $scope.saveContact = contact => $scope.contactList.$save(contact)
+    
+    $scope.removeContact = contact => {
+        if(confirm("Are you sure you want to delete this contact?")){
+    $scope.contactList.$remove(contact)
+        }
     }
     
-    $scope.saveEditTodo = function(){
-        $scope.editing = false
-        apptodoLocalStorage.set('item', $scope.todoitemsList)
-    }
     
-}]) 
+})
+
+

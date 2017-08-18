99d2c0cec6c762361ce749380bb3fcf705cb33f7
emilyb22 <emilyb22@live.ca>
2017-08-18 15:42:02 -0400

    finishing touches, adding editing to priority, switching the naming, making under 300 responsive

--- app.js
+++ app.js
@@ -1,43 +1,34 @@
-angular
-	.module('contactsApp', ['firebase'])
-    .constant('firebaseConfig', {
-     apiKey: "AIzaSyBl1hDAyp38K3WNeZljOl5Z4fMGayyLSi8",
-    authDomain: "final-f22f0.firebaseapp.com",
-    databaseURL: "https://final-f22f0.firebaseio.com",
-    projectId: "final-f22f0",
-    storageBucket: "final-f22f0.appspot.com",
-    messagingSenderId: "149695883160"  
-})
-.run(firebaseConfig => firebase.initializeApp(firebaseConfig))
-.controller('contactsCtrl', function($scope, $firebaseObject, $firebaseArray){
-	const dbRef = firebase.database().ref().child('contacts')
-    
-    $scope.contactList = $firebaseArray(dbRef)    
-    this.getBlankContact = () => ({
-        fname:'',
-        lname: '',
-        phone: '',
-        subject: '',
-        priority: '',
-        isCompleted: false
+angular.module('TodoApp', ['firebase']).constant('firebaseConfig', {
+    apiKey: "AIzaSyBl1hDAyp38K3WNeZljOl5Z4fMGayyLSi8"
+    , authDomain: "final-f22f0.firebaseapp.com"
+    , databaseURL: "https://final-f22f0.firebaseio.com"
+    , projectId: "final-f22f0"
+    , storageBucket: "final-f22f0.appspot.com"
+    , messagingSenderId: "149695883160"
+}).run(firebaseConfig => firebase.initializeApp(firebaseConfig)).controller('todosCtrl', function ($scope, $firebaseObject, $firebaseArray) {
+    const dbRef = firebase.database().ref().child('todos')
+    $scope.todoList = $firebaseArray(dbRef)
+    this.getBlankTodo = () => ({
+        fname: ''
+        , lname: ''
+        , phone: ''
+        , subject: ''
+        , priority: ''
+        , isCompleted: false
     })
-    
-    $scope.newContact = this.getBlankContact()
-    
-    $scope.addContact = () => {
-        $scope.contactList.$add($scope.newContact)
-        $scope.newContact = this.getBlankContact()
+    $scope.newTodo = this.getBlankTodo()
+    $scope.addTodo = () => {
+        $scope.todoList.$add($scope.newTodo)
+        $scope.newTodo = this.getBlankTodo()
     }
-    
-    $scope.saveContact = contact => $scope.contactList.$save(contact)
-    
-    $scope.removeContact = contact => {
-        if(confirm("Are you sure you want to delete this contact?")){
-    $scope.contactList.$remove(contact)
+    $scope.saveTodo = todo => $scope.todoList.$save(todo)
+    $scope.removeTodo = todo => {
+        if (confirm("Are you sure you want to delete this todo?")) {
+            $scope.todoList.$remove(todo)
         }
     }
-    
-    
+    $scope.check = function (todo) {
+        todo.isCompleted = !todo.isCompleted;
+        $scope.saveTodo(todo)
+    }
 })
-
-
